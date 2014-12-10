922ca0370bc5ca976afeaaf8fee1bbf0540521f1
Simon Kohlstrom <simon.kohlstrom@dri-nordic.com>
2014-12-10 15:02:41 +0100

    Added utillity functions for multibyte str replace
    Signed-off-by: Simon Kohlstrom <simon.kohlstrom@dri-nordic.com>

---
+++ src/Utils/StringUtils.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace DRI\SugarCRM\Utils;
+
+/**
+ * @author Simon Köhlström
+ */
+class StringUtils {
+
+    /**
+     * @param $search
+     * @param $replace
+     * @param $subject
+     * @param int $count
+     * @return array|string
+     */
+    public static function mbStrReplace($search, $replace, $subject, &$count = 0) {
+        if (!is_array($subject)) {
+            // Normalize $search and $replace so they are both arrays of the same length
+            $searches = is_array($search) ? array_values($search) : array($search);
+            $replacements = is_array($replace) ? array_values($replace) : array($replace);
+            $replacements = array_pad($replacements, count($searches), '');
+
+            foreach ($searches as $key => $search) {
+                $parts = mb_split(preg_quote($search), $subject);
+                $count += count($parts) - 1;
+                $subject = implode($replacements[$key], $parts);
+            }
+        } else {
+            // Call mb_str_replace for each subject in array, recursively
+            foreach ($subject as $key => $value) {
+                $subject[$key] = self::mbStrReplace($search, $replace, $value, $count);
+            }
+        }
+
+        return $subject;
+    }
+
+    /**
+     * @param $string
+     * @param $replacement
+     * @param $start
+     * @param null $length
+     * @return string
+     */
+    public static function mbSubStrReplace($string, $replacement, $start, $length=NULL) {
+        if (is_array($string)) {
+            $num = count($string);
+            // $replacement
+            $replacement = is_array($replacement) ? array_slice($replacement, 0, $num) : array_pad(array($replacement), $num, $replacement);
+            // $start
+            if (is_array($start)) {
+                $start = array_slice($start, 0, $num);
+                foreach ($start as $key => $value)
+                    $start[$key] = is_int($value) ? $value : 0;
+            }
+            else {
+                $start = array_pad(array($start), $num, $start);
+            }
+            // $length
+            if (!isset($length)) {
+                $length = array_fill(0, $num, 0);
+            }
+            elseif (is_array($length)) {
+                $length = array_slice($length, 0, $num);
+                foreach ($length as $key => $value)
+                    $length[$key] = isset($value) ? (is_int($value) ? $value : $num) : 0;
+            }
+            else {
+                $length = array_pad(array($length), $num, $length);
+            }
+            // Recursive call
+            return array_map(__METHOD__, $string, $replacement, $start, $length);
+        }
+        preg_match_all('/./us', (string)$string, $smatches);
+        preg_match_all('/./us', (string)$replacement, $rmatches);
+        if ($length === NULL) $length = mb_strlen($string);
+        array_splice($smatches[0], $start, $length, $rmatches[0]);
+        return implode("", $smatches[0]);
+    }
+
+}
